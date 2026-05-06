22d1f63ca449e4974041ee9d4e088ac0a75b8f0e
ritesh <riteshsaini1601@gmail.com>
2026-05-06 21:56:08 +0530

    Security files are removed and important files are added

--- vector_db.py
+++ vector_db.py
@@ -1,16 +1,22 @@
+import os
 import threading
 
+from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
 
+
+load_dotenv()
 
 qdrant_lock = threading.RLock()
 
 
 class QdrantStorage:
-    def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
-        self.client = QdrantClient(url=url, timeout=30)
-        self.collection = collection
+    def __init__(self, url=None, collection=None, dim=3072):
+        qdrant_url = url or os.getenv("QDRANT_URL", "http://localhost:6333")
+        qdrant_api_key = os.getenv("QDRANT_API_KEY")
+        self.client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key, timeout=30)
+        self.collection = collection or os.getenv("QDRANT_COLLECTION", "docs")
         with qdrant_lock:
             if not self.client.collection_exists(self.collection):
                 self.client.create_collection(
